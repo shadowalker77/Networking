136aa4256873c8c060a74a6b1f9b5c27bfe66793
shadoWalker7 <alireza.b@aut.ac.ir>
2019-04-14 17:31:12 +0430

    0.0.7

--- ayannetworking/src/main/java/ir/ayantech/ayannetworking/networking/RetrofitClient.kt
+++ ayannetworking/src/main/java/ir/ayantech/ayannetworking/networking/RetrofitClient.kt
@@ -3,6 +3,7 @@ package ir.ayantech.ayannetworking.networking
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.net.Proxy
 import java.util.concurrent.TimeUnit
 
 class RetrofitClient private constructor() {
@@ -29,6 +30,7 @@ class RetrofitClient private constructor() {
             okHttpClientBuilder.connectTimeout(timeout, TimeUnit.SECONDS)
             okHttpClientBuilder.readTimeout(timeout, TimeUnit.SECONDS)
             okHttpClientBuilder.writeTimeout(timeout, TimeUnit.SECONDS)
+            okHttpClientBuilder.proxy(Proxy.NO_PROXY)
             return okHttpClient ?: okHttpClientBuilder.build().also { okHttpClient = it }
         }
     }
